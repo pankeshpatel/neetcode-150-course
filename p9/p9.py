0946bbb6c191621004400bff17dfa7963dfc330f
pankesh.patel <pankesh.patel@bolt-data.com>
2025-02-27 19:50:05 -0500

    solved p9

---
+++ p9/p9.py
@@ -0,0 +1,41 @@
+# Longest Consecutive Sequence
+# Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+
+# A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element.
+# The elements do not have to be consecutive in the original array.
+
+# You must write an algorithm that runs in O(n) time.
+
+# Example 1:
+
+# Input: nums = [2,20,4,10,3,4,5]
+
+# Output: 4
+# Explanation: The longest consecutive sequence is [2, 3, 4, 5].
+
+# Example 2:
+
+# Input: nums = [0,3,2,5,4,6,1,1]
+
+# Output: 7
+
+# nums = [2, 20, 4, 10, 3, 4, 5]
+
+nums = [9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6]
+
+if len(nums) == 0:
+    last_position = 0
+
+
+numSet = sorted(set(nums))
+print(numSet)
+longest = 0
+
+for num in numSet:
+    if (num - 1) not in numSet:
+        length = 1
+        while (num + length) in numSet:
+            length += 1
+        longest = max(length, longest)
+
+print(f"longest ... {longest}")
