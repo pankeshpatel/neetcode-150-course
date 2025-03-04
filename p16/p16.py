6a449e7e9d20abe0d31c36239e805007a04ba96b
pankesh.patel <pankesh.patel@bolt-data.com>
2025-03-03 20:14:50 -0500

    added p16

---
+++ p16/p16.py
@@ -0,0 +1,53 @@
+# Permutation in String
+# You are given two strings s1 and s2.
+
+# Return true if s2 contains a permutation of s1, or false otherwise.
+# That means if a permutation of s1 exists as a substring of s2, then return true.
+
+# Both strings only contain lowercase letters.
+
+# Example 1:
+
+# Input: s1 = "abc", s2 = "lecabee"
+
+# Output: true
+# Explanation: The substring "cab" is a permutation of "abc" and is present in "lecabee".
+
+# Example 2:
+
+# Input: s1 = "abc", s2 = "lecaabee"
+
+# Output: false
+
+s1 = "ab"
+s2 = "lecabee"
+
+
+def createHashMap(input_string):
+    hashMapString = {}
+    for s in input_string:
+        if s in hashMapString:
+            hashMapString[s] += 1
+        else:
+            hashMapString[s] = 1
+
+    return hashMapString
+
+
+hashMapSubString = createHashMap(s1)
+
+l = 0
+r = len(s1)
+subStringFlag = False
+
+
+while r <= len(s2):
+    substring = createHashMap(s2[l:r])
+    if substring == hashMapSubString:
+        subStringFlag = True
+        break
+    else:
+        l = l + 1
+    r = r + 1
+
+print(f"{subStringFlag}")
