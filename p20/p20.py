a4719d61f213695c9bc2559b28d7dd7a6b42b8b0
pankesh.patel <pankesh.patel@bolt-data.com>
2025-03-05 13:27:32 -0500

    added p20

---
+++ p20/p20.py
@@ -0,0 +1,36 @@
+# Generate Parentheses
+# You are given an integer n. Return all well-formed parentheses strings that you can generate with n pairs of parentheses.
+
+# Example 1:
+
+# Input: n = 1
+
+# Output: ["()"]
+# Example 2:
+
+# Input: n = 3
+
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
